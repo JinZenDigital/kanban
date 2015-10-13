58fc52b852edf4345c6ba615a7fa8724db5a0f61
Xi Jin <jin@zendigital.com>
2015-10-13 15:43:35 -0400

    this is it

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  entry: APP_PATH,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port:4112,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: APP_PATH
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: APP_PATH
+      }
+    ]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
